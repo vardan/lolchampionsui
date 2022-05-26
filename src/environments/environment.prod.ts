f94db08ef2074728dff0c5d2a41a41f3cd945219
Vardan Saakian <saakyanv@gmail.com>
2022-05-26 13:11:27 +0200

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: "https://lol-champions-os-project.apps.ose.openshift.conygre.com/api/v1/lolchampion"
+  apiUrl: "https://lol-champions-os-project.apps.ose.openshift.conygre.com/api/v1/lolchampion/"
 };
